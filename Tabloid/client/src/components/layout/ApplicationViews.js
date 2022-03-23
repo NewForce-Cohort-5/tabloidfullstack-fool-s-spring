8f2e5bf11d0a4ea7ab3d5fdb8a7a321c03c3caeb
Andrew Birt <abirt1986@gmail.com>
2022-03-23 14:08:35 -0400

    add route to the application views for viewing a single post

--- Tabloid/client/src/components/layout/ApplicationViews.js
+++ Tabloid/client/src/components/layout/ApplicationViews.js
@@ -7,6 +7,7 @@ import MyPostList from "../post/MyPostList";
 import PostList from "../post/PostList";
 import Hello from "./Hello";
 import TagList from "../tag/TagList"
+import PostDetails from "../post/PostDetails";
 
 export default function ApplicationViews() {
   const { isLoggedIn } = useContext(UserProfileContext);
@@ -26,6 +27,7 @@ export default function ApplicationViews() {
         <Route path="/" element={<Hello />} />
         <Route path="/posts" element={<PostList />} />
         <Route path="/myposts" element={<MyPostList />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
         <Route path="/tags" element={<TagList />} />
       </Routes>
    );
