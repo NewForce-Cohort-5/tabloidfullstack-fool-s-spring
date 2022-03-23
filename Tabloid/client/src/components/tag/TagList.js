bc899ac1f5d546fb9e5250f3ac598e968a91db48
helloamandaball <helloamandaball@gmail.com>
2022-03-23 08:50:39 -0400

    ticket #13-View All Tags completed

--- Tabloid/client/src/components/tag/TagList.js
+++ Tabloid/client/src/components/tag/TagList.js
@@ -12,16 +12,8 @@ const TagList = () => {
     return (
         <div className="container pt-5">
             <h1>Tags</h1>
-            <p>
-                {/* <a class="btn btn-info" asp-action="Create">New Tag</a> */}
-            </p>
+
             <table class="table table-striped mt-3">
-                {/* <thead>
-                    <tr>
-                        <th>Tag Name:</th>
-                        <th></th>
-                    </tr>
-                </thead> */}
                 <tbody>
                     {tags.map((tag) => (
                         <Tag key={tag.id} tagProp={tag} />
