f0667ee4cc8197cb3a665d31f786d1218d35fafa
helloamandaball <helloamandaball@gmail.com>
2022-03-24 10:58:40 -0400

    merge with new main

--- Tabloid/client/src/components/tag/Tag.js
+++ Tabloid/client/src/components/tag/Tag.js
@@ -13,27 +13,8 @@ export const Tag = ({ tagProp }) => {
     //if confirmDelete button is clicked ask if they're sure to delete, then handleDelete button is clicked to confirm then "deleteTag(tagProp.id).then(getAllTags);" then getAllTags
     const confirmDelete = () => {
         return (
-            // <div className="modal fade" tabindex="-1" role="dialog" id="myModal">
-                {/* <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">Modal title</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        <p>Are you sure you want to delete #{tagProp.name}?</p>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                        <button type="button" className="btn btn-success" id="tagProp.id" onClick={handleDelete}>
-                            <span>Yes, Delete</span>
-                        </button>
-                    </div>
-                    </div>
-                </div>
-            </div> */}
+            <>
+            </>
         )
     }
    
@@ -49,9 +30,7 @@ export const Tag = ({ tagProp }) => {
                     <button type="button" className="btn btn-link" id="tagProp.id" data-toggle="modal" data-target="#myModal" onClick={confirmDelete}>
                         <span>&#128465;</span>
                     </button>
-                    {/* <button type="button" className="btn btn-link" id="tagProp.id" onClick={handleDelete}>
-                        <span>Yes, Delete</span>
-                    </button> */}
+                    
 
                     <div>
                         <Button color="danger" onClick={function noRefCheck(){}}>
