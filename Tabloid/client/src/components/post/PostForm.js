ffb0be8e97b98c62730806af7cb571fbd4160183
Andrew Birt <abirt1986@gmail.com>
2022-03-24 16:26:55 -0400

    display invlaid fields when users tries to make an invalid post

--- Tabloid/client/src/components/post/PostForm.js
+++ Tabloid/client/src/components/post/PostForm.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
+import { Button, Container, Form, FormFeedback, FormGroup, Input, Label } from "reactstrap";
 import { PostContext } from "../../providers/PostProvider";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -22,6 +22,36 @@ const PostForm = () => {
 
   const { addNewPost, getPostToEdit, editPost } = useContext(PostContext);
 
+  const [invalidFields, setInvalidFields] = useState({
+    title: false,
+    content: false,
+    categoryId: false
+   });
+
+   const setInvalid = (failed) => {
+
+    const newInvalidFields = { ...invalidFields };
+  
+    //* For each validation error display it's message in the field which is invalid.
+    for (const invalidType of Object.keys(failed.errors)) {
+      const invalidField = invalidType;
+  
+      //? ImageLocation => imageLocation
+      const fieldId = invalidField.replace(/^[A-Z]/, invalidField[0].toLowerCase());
+  
+      //? ex. The field Profile Image URL must be a string or array type with a maximum length of '255'
+      const message = failed.errors[invalidType][0].replace(invalidField, document.getElementById(fieldId).previousElementSibling.innerText);
+      
+      document.getElementById(fieldId).nextElementSibling.innerText = message;
+      
+      newInvalidFields[fieldId] = true;
+      
+    }
+  
+    setInvalidFields(newInvalidFields);
+   };
+
+
   const { id } = useParams();
 
   const navigate = useNavigate();
@@ -34,10 +64,18 @@ const PostForm = () => {
 
   const handleSubmitPost = () => {
     if (id) {
-      editPost(post).then(() => navigate(`/posts/${id}`));
+      editPost(post).then((r) => {
+        // navigate(`/posts/${id}`);
+      });
     } else {
       addNewPost({ ...post, createDateTime: new Date().toISOString() })
-      .then(() => navigate("/"));
+      .then((r) => {
+        if (r.status === 400) {
+          setInvalid(r);
+        } else {
+          navigate(`/posts/${id}`);
+        }
+      });
     }
   };
 
@@ -66,46 +104,54 @@ const PostForm = () => {
     <Container className="pt-5">
       <h2>{action} Post</h2>
       <Form >
-        <FormGroup floating>
+        <FormGroup >
+          <Label for="title" hidden>Title</Label>
           <Input
             id="title"
             placeholder="Title"
             onChange={handleChangeInput}
+            invalid={invalidFields.title}
             value={post.title}
             />
-          <Label for="title" hidden>Title</Label>
+          <FormFeedback></FormFeedback>
         </FormGroup>
         <FormGroup >
+          <Label for="content" hidden>Content</Label>
           <Input
             id="content"
             placeholder="Content"
             onChange={handleChangeInput}
+            invalid={invalidFields.content}
             value={post.content}
             type="textarea"
             />
-          <Label for="content" hidden>Content</Label>
+          <FormFeedback></FormFeedback>
         </FormGroup>
         <FormGroup >
+          <Label for="imageLocation" hidden>Image Location</Label>
           <Input
             id="imageLocation"
             placeholder="Image Location"
             onChange={handleChangeInput}
+            invalid={invalidFields.imageLocation}
             value={post.imageLocation}
             />
-          <Label for="imageLocation" hidden>Image Location</Label>
+          <FormFeedback></FormFeedback>
         </FormGroup>
         <FormGroup >
+          <Label for="CategoryId" hidden>Category</Label>
           <Input
             id="categoryId"
             placeholder="Category"
             type="select"
             onChange={handleChangeInput}
+            invalid={invalidFields.categoryId}
             value={post.categoryId}
             >
               <option>Please Select a Category</option>
               {categories.map(c => <option value={c.id}>{c.name}</option>)}
-            </Input>
-          <Label for="CategoryId" hidden>Category</Label>
+          </Input>
+          <FormFeedback></FormFeedback>
         </FormGroup>
         <Button onClick={handleSubmitPost}>Submit</Button>
       </Form>
