5ad73ca353ec7a4ebe67fd4e3d588ec7b3647d09
helloamandaball <helloamandaball@gmail.com>
2022-03-24 13:34:08 -0400

    ticket 15 - delete a tag completed

--- Tabloid/client/src/components/tag/Tag.js
+++ Tabloid/client/src/components/tag/Tag.js
@@ -1,23 +1,26 @@
 import React, { useContext } from "react";
-import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from "reactstrap/lib/Button";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { TagContext } from "../../providers/TagProvider";
 
 export const Tag = ({ tagProp }) => {
     const { getAllTags, deleteTag } = useContext(TagContext);
- 
+    
     const handleDelete = () => {
-        deleteTag(tagProp.id)
-        .then(getAllTags);
+        var confirmDelete = window.confirm("Are you sure you want to delete the tag: " + (tagProp.name) + "?")
+        if (confirmDelete) {
+            deleteTag(tagProp.id)
+            .then(getAllTags);;
+        } else {
+            getAllTags();
+        };
+        
+        //Use this code if you're doing a straight delete without a confirmation window:
+        // const handleDelete = () => {
+            //     deleteTag(tagProp.id)
+            //     .then(getAllTags);
+            // }
     }
-    //if confirmDelete button is clicked ask if they're sure to delete, then handleDelete button is clicked to confirm then "deleteTag(tagProp.id).then(getAllTags);" then getAllTags
-    const confirmDelete = () => {
-        return (
-            <>
-            </>
-        )
-    }
-   
 
     return (
         <>
@@ -26,35 +29,11 @@ export const Tag = ({ tagProp }) => {
                     <h5>#{tagProp.name}</h5>
                 </td>
                 <td className="col-2">
-
-                    <button type="button" className="btn btn-link" id="tagProp.id" data-toggle="modal" data-target="#myModal" onClick={confirmDelete}>
-                        <span>&#128465;</span>
+               
+                    <button type="button" className="btn btn-link" id="tagProp.id" onClick={handleDelete}>
+                        <FontAwesomeIcon icon={faTrashAlt} className="text-secondary"/>
                     </button>
-                    
-
-                    <div>
-                        <Button color="danger" onClick={function noRefCheck(){}}>
-                            Click Me
-                        </Button>
-                        <Modal fade={false} toggle={function noRefCheck(){}}>
-                            <ModalHeader toggle={function noRefCheck(){}}>
-                                Confirm Delete
-                            </ModalHeader>
-                            <ModalBody>
-                                Are you sure you want to delete #{tagProp.name}?
-                            </ModalBody>
-                            <ModalFooter>
-                                <Button color="primary" onClick={function noRefCheck(){}}>
-                                    Yes, Delete
-                                </Button>
-                                {' '}
-                                <Button onClick={function noRefCheck(){}}>
-                                    Cancel
-                                </Button>
-                            </ModalFooter>
-                        </Modal>
-                    </div>
-                </td>
+                </td>  
             </tr>
         </>
     );
