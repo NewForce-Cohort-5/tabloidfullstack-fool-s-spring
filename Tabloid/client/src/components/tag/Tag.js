e03f6e3966dae1bc4655217e968cc4fa26b45586
helloamandaball <helloamandaball@gmail.com>
2022-03-24 10:24:05 -0400

    saving point

--- Tabloid/client/src/components/tag/Tag.js
+++ Tabloid/client/src/components/tag/Tag.js
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+import {Modal, Button} from "react-bootstrap";
+
 import { TagContext } from "../../providers/TagProvider";
 
 export const Tag = ({ tagProp }) => {
@@ -11,8 +13,8 @@ export const Tag = ({ tagProp }) => {
     //if confirmDelete button is clicked ask if they're sure to delete, then handleDelete button is clicked to confirm then "deleteTag(tagProp.id).then(getAllTags);" then getAllTags
     const confirmDelete = () => {
         return (
-            <div className="modal fade" tabindex="-1" role="dialog" id="myModal">
-                <div className="modal-dialog" role="document">
+            // <div className="modal fade" tabindex="-1" role="dialog" id="myModal">
+                {/* <div className="modal-dialog" role="document">
                     <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title">Modal title</h5>
@@ -31,7 +33,7 @@ export const Tag = ({ tagProp }) => {
                     </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         )
     }
    
@@ -50,6 +52,21 @@ export const Tag = ({ tagProp }) => {
                     {/* <button type="button" className="btn btn-link" id="tagProp.id" onClick={handleDelete}>
                         <span>Yes, Delete</span>
                     </button> */}
+
+                    <Modal.Dialog>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal title</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <p>Modal body text goes here.</p>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="secondary">Close</Button>
+                        <Button variant="primary">Save changes</Button>
+                    </Modal.Footer>
+                    </Modal.Dialog>
                 </td>
             </tr>
         </>
