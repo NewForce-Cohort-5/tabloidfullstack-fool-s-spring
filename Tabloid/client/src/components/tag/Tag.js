475fcff4a0637425dfa4182a76d80874797f90e5
garyc <garyclayton02@yahoo.com>
2022-03-29 14:06:01 -0400

    added to original PR

--- Tabloid/client/src/components/tag/Tag.js
+++ Tabloid/client/src/components/tag/Tag.js
@@ -1,10 +1,15 @@
 import React, { useContext } from "react";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 import { TagContext } from "../../providers/TagProvider";
+import { useNavigate } from "react-router-dom"
 
 export const Tag = ({ tagProp }) => {
     const { getAllTags, deleteTag } = useContext(TagContext);
+    const navigate = useNavigate()
+    const handleEdit = () => {
+        navigate(`/tags/edit/${tagProp.id}`)
+    };
     
     const handleDelete = () => {
         var confirmDelete = window.confirm("Are you sure you want to delete the tag: " + (tagProp.name) + "?")
@@ -28,10 +33,13 @@ export const Tag = ({ tagProp }) => {
                 <td className="col-10 align-middle">
                     <h5>#{tagProp.name}</h5>
                 </td>
-                <td className="col-2">
-               
+                <td className="col-2 text-right">
+                    <button type="button" className="btn btn-link" id="tagProp.id" onClick={handleEdit}>
+                        <FontAwesomeIcon icon={faPencilAlt} className="text-secondary"/>
+                    </button>
                     <button type="button" className="btn btn-link" id="tagProp.id" onClick={handleDelete}>
-                        
+                        <FontAwesomeIcon icon={faTrashAlt} className="text-secondary"/>
+
                     </button>
                 </td>  
             </tr>
